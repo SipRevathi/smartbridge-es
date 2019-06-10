f22c07260284f97d8c9b574a6116ce7642f59456
SipRevathi <51318728+SipRevathi@users.noreply.github.com>
2019-06-10 10:42:48 +0530

    Add files via upload

---
+++ day 3/g and s.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	int y;
+	printf("enter the values of x and y");
+	scanf("%d%d",&x,&y);
+	if(x>y)
+	printf("y is less then x");
+	else
+	printf("y is greater then x");
+	
+}
